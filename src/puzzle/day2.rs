28c8198de32fe2ecd31ba35a79fb509d17bf2b45
Romain Delamare <romain@delama.re>
2020-12-02 12:29:27 +0100

    day 2, part 2

--- src/puzzle/day2.rs
+++ src/puzzle/day2.rs
@@ -7,25 +7,48 @@ use crate::puzzle::input::read_lines;
 pub fn execute() {
     let entries = read_lines("day2").unwrap();
     println!(
-        "2:1 — Number of valid passwords: {}",
-        count_valid_passwords(entries)
+        "2:1 — Number of valid passwords for sled policy: {}",
+        count_valid_passwords_for_sled_policy(&entries)
+    );
+    println!(
+        "2:2 — Number of valid passwords for toboggan policy: {}",
+        count_valid_passwords_for_toboggan_policy(&entries)
     );
 }
 
-fn count_valid_passwords(entries: Vector<String>) -> usize {
+fn count_valid_passwords_for_sled_policy(entries: &Vector<String>) -> usize {
     entries
         .iter()
-        .map(|entry| parse_password_line(entry.as_str()))
+        .map(|entry| parse_password_line_to_sled_policy(entry.as_str()))
         .filter(|(password, policy)| policy.is_valid(password.as_str()))
         .count()
 }
 
-fn parse_password_line(password_line: &str) -> (String, PasswordPolicy) {
+fn parse_password_line_to_sled_policy(password_line: &str) -> (String, SledPasswordPolicy) {
     let (password, letter, start, end) = split_password_line(password_line);
-    (password, PasswordPolicy::new(letter, start, end))
+    (
+        password,
+        SledPasswordPolicy::new(letter, start as u32, end as u32),
+    )
 }
 
-fn split_password_line(password_line: &str) -> (String, char, u32, u32) {
+fn count_valid_passwords_for_toboggan_policy(entries: &Vector<String>) -> usize {
+    entries
+        .iter()
+        .map(|entry| parse_password_line_to_toboggan_policy(entry.as_str()))
+        .filter(|(password, policy)| policy.is_valid(password.as_str()))
+        .count()
+}
+
+fn parse_password_line_to_toboggan_policy(password_line: &str) -> (String, TobogganPasswordPolicy) {
+    let (password, letter, first_position, second_position) = split_password_line(password_line);
+    (
+        password,
+        TobogganPasswordPolicy::new(letter, first_position, second_position),
+    )
+}
+
+fn split_password_line(password_line: &str) -> (String, char, usize, usize) {
     let captures = PASSWORD_LINE_REGEX
         .captures(password_line)
         .unwrap_or_else(|| panic!("Invalid password line: {}", password_line));
@@ -38,25 +61,24 @@ fn split_password_line(password_line: &str) -> (String, char, u32, u32) {
             .chars()
             .next()
             .unwrap(),
-        captures.name("start").unwrap().as_str().parse().unwrap(),
-        captures.name("end").unwrap().as_str().parse().unwrap(),
+        captures.name("a").unwrap().as_str().parse().unwrap(),
+        captures.name("b").unwrap().as_str().parse().unwrap(),
     )
 }
 
 lazy_static! {
     static ref PASSWORD_LINE_REGEX: Regex =
-        Regex::new(r"(?P<start>\d+)-(?P<end>\d+) (?P<letter>[[:alpha:]]): (?P<password>.*)")
-            .unwrap();
+        Regex::new(r"(?P<a>\d+)-(?P<b>\d+) (?P<letter>[[:alpha:]]): (?P<password>.*)").unwrap();
 }
 
 #[derive(PartialEq, Debug)]
-struct PasswordPolicy {
+struct SledPasswordPolicy {
     letter: char,
     start: u32,
     end: u32,
 }
 
-impl PasswordPolicy {
+impl SledPasswordPolicy {
     fn new(letter: char, start: u32, end: u32) -> Self {
         Self { letter, start, end }
     }
@@ -67,75 +89,133 @@ impl PasswordPolicy {
     }
 }
 
+struct TobogganPasswordPolicy {
+    letter: char,
+    first_position: usize,
+    second_position: usize,
+}
+
+impl TobogganPasswordPolicy {
+    fn new(letter: char, first_position: usize, second_position: usize) -> Self {
+        Self {
+            letter,
+            first_position,
+            second_position,
+        }
+    }
+
+    fn is_valid(&self, password: &str) -> bool {
+        let chars = chars_at(password, self.first_position - 1, self.second_position - 1);
+        match chars {
+            (a, b) if a == b => false,
+            (a, b) if a == self.letter || b == self.letter => true,
+            _ => false,
+        }
+    }
+}
+
+fn chars_at(str: &str, first_position: usize, second_position: usize) -> (char, char) {
+    (
+        str.chars().nth(first_position).unwrap(),
+        str.chars().nth(second_position).unwrap(),
+    )
+}
+
 #[cfg(test)]
-mod password_policy_is_valid_should {
-    use super::PasswordPolicy;
+mod sled_password_policy_is_valid_should {
+    use super::SledPasswordPolicy;
 
     #[test]
     fn return_true_when_policy_is_one_to_three_as_and_there_is_one_a() {
-        let policy = PasswordPolicy::new('a', 1, 3);
+        let policy = SledPasswordPolicy::new('a', 1, 3);
         assert!(policy.is_valid("abcde"))
     }
 
     #[test]
     fn return_true_when_policy_is_one_to_three_as_and_there_are_two_as() {
-        let policy = PasswordPolicy::new('a', 1, 3);
+        let policy = SledPasswordPolicy::new('a', 1, 3);
         assert!(policy.is_valid("abacde"))
     }
 
     #[test]
     fn return_true_when_policy_is_one_to_three_as_and_there_are_three_as() {
-        let policy = PasswordPolicy::new('a', 1, 3);
+        let policy = SledPasswordPolicy::new('a', 1, 3);
         assert!(policy.is_valid("abacade"))
     }
 
     #[test]
     fn return_false_when_policy_is_one_to_three_as_and_there_are_no_as() {
-        let policy = PasswordPolicy::new('a', 1, 3);
+        let policy = SledPasswordPolicy::new('a', 1, 3);
         assert!(!policy.is_valid("bcde"))
     }
 
     #[test]
     fn return_false_when_policy_is_one_to_three_as_and_there_are_four_as() {
-        let policy = PasswordPolicy::new('a', 1, 3);
+        let policy = SledPasswordPolicy::new('a', 1, 3);
         assert!(!policy.is_valid("abacadae"))
     }
 
     #[test]
     fn return_false_when_policy_is_one_to_three_bs_and_there_are_no_bs() {
-        let policy = PasswordPolicy::new('b', 1, 3);
+        let policy = SledPasswordPolicy::new('b', 1, 3);
         assert!(!policy.is_valid("cdefg"))
     }
 
     #[test]
     fn return_true_when_policy_is_two_to_nine_cs_and_there_are_nine_cs() {
-        let policy = PasswordPolicy::new('c', 2, 9);
+        let policy = SledPasswordPolicy::new('c', 2, 9);
         assert!(policy.is_valid("ccccccccc"))
     }
 }
 
 #[cfg(test)]
-mod parse_password_line_should {
-    use super::{parse_password_line, PasswordPolicy};
+mod parse_password_line_to_sled_policy {
+    use super::{parse_password_line_to_sled_policy, SledPasswordPolicy};
 
     #[test]
     fn parse_first_example() {
-        let result = parse_password_line("1-3 a: abcde");
+        let result = parse_password_line_to_sled_policy("1-3 a: abcde");
 
-        assert_eq!(result, ("abcde".into(), PasswordPolicy::new('a', 1, 3)));
+        assert_eq!(result, ("abcde".into(), SledPasswordPolicy::new('a', 1, 3)));
     }
 
     #[test]
     fn parse_second_example() {
-        let result = parse_password_line("1-3 b: cdefg");
+        let result = parse_password_line_to_sled_policy("1-3 b: cdefg");
 
-        assert_eq!(result, ("cdefg".into(), PasswordPolicy::new('b', 1, 3)));
+        assert_eq!(result, ("cdefg".into(), SledPasswordPolicy::new('b', 1, 3)));
     }
 
     #[test]
     fn parse_third_example() {
-        let result = parse_password_line("2-9 c: ccccccccc");
+        let result = parse_password_line_to_sled_policy("2-9 c: ccccccccc");
 
-        assert_eq!(result, ("ccccccccc".into(), PasswordPolicy::new('c', 2, 9)));
+        assert_eq!(
+            result,
+            ("ccccccccc".into(), SledPasswordPolicy::new('c', 2, 9))
+        );
+    }
+}
+
+#[cfg(test)]
+mod toboggan_password_policy_is_valid_should {
+    use super::TobogganPasswordPolicy;
+
+    #[test]
+    fn return_true_when_letter_is_exactly_at_one_position() {
+        let policy = TobogganPasswordPolicy::new('a', 1, 3);
+        assert!(policy.is_valid("abcde"))
+    }
+
+    #[test]
+    fn return_false_when_letter_is_not_at_any_position() {
+        let policy = TobogganPasswordPolicy::new('b', 1, 3);
+        assert!(!policy.is_valid("cdefg"))
+    }
+
+    #[test]
+    fn return_false_when_letter_is_at_both_positions() {
+        let policy = TobogganPasswordPolicy::new('c', 2, 9);
+        assert!(!policy.is_valid("ccccccccc"))
     }
 }
